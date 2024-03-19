fa71a457caa109ba41d35d5454cd8c32f7240b83
creative-darkstar <159861706+creative-darkstar@users.noreply.github.com>
2024-03-20 03:00:16 +0900

    [level 0] Title: 안전지대, Time: 0.07 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -5,10 +5,10 @@ def solution(board):
     for row_idx in range(n):
         for col_idx in range(n):
             if board[row_idx][col_idx] == 1:
-                row_start = 0 if row_idx - 1 < 0 else -1
-                row_end = 0 if row_idx + 1 > n - 1 else 1
-                col_start = 0 if col_idx - 1 < 0 else -1
-                col_end = 0 if col_idx + 1 > n - 1 else 1
+                row_start = 0 if row_idx == 0 else -1
+                row_end = 0 if row_idx == n - 1 else 1
+                col_start = 0 if col_idx == 0 else -1
+                col_end = 0 if col_idx == n - 1 else 1
                 for i in range(row_idx + row_start, row_idx + row_end + 1):
                     for j in range(col_idx + col_start, col_idx + col_end + 1):
                         danger_zone.add((i, j))
